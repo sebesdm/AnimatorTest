7fba5b610594b0e14a4c9de87a24791e15d6e7c2
sebesdm <dave@davesebesta.com>
2023-04-29 16:45:52 -0400

    Separate animations

--- Assets/SetupAnimatorsOnModels.cs
+++ Assets/SetupAnimatorsOnModels.cs
@@ -7,12 +7,20 @@ public class SetupAnimatorsOnModels : MonoBehaviour
 
     void Awake()
     {
+        int i = 1;
+
         var characters = GameObject.FindObjectsOfType<Character>();
         foreach (var character in characters)
         {
             Animator animator = character.gameObject.AddComponent<Animator>();
             animator.runtimeAnimatorController = controller;
             animator.avatar = avatar;
+
+            if (i == 1)
+            {
+                i++;
+                animator.SetBool("Shoot_b", true);  // Added a run animation to the animation controller, setting this for only one model
+            }
         }
     }
 }
