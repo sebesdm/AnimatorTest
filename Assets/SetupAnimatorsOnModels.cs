fd6710eee40cbc0db58fb124552731c9381fac2c
sebesdm <dave@davesebesta.com>
2023-04-29 10:41:39 -0400

    test project

---
+++ Assets/SetupAnimatorsOnModels.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SetupAnimatorsOnModels : MonoBehaviour
+{
+    public RuntimeAnimatorController controller;
+    public Avatar avatar;
+
+    void Awake()
+    {
+        var characters = GameObject.FindObjectsOfType<Character>();
+        foreach (var character in characters)
+        {
+            Animator animator = character.gameObject.AddComponent<Animator>();
+            animator.runtimeAnimatorController = controller;
+            animator.avatar = avatar;
+        }
+    }
+}
